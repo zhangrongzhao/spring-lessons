475f16c32a2796c7f287ea01a344a88738630841
zhangrongzhao <rongzhao.zhang@allcure.cn>
2020-07-30 11:15:59 +0800

    bean：使用单例注册接口SingletonRegistry注册单例

--- thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/BeanSingletonRegistryDemo.java
+++ thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/BeanSingletonRegistryDemo.java
@@ -3,6 +3,7 @@ package org.geekbang.thinking.in.spring.bean.definition;
 import org.geekbang.thinking.in.spring.bean.definition.factory.DefaultUserFactory;
 import org.geekbang.thinking.in.spring.bean.definition.factory.UserFactory;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.SingletonBeanRegistry;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,15 +13,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class BeanSingletonRegistryDemo {
 
     public static void main(String[] args){
+        /*1.BeanDefinitionRegistry  Bean定义登记簿*/
+        /*2.SingletonRegistry 单例登记簿*/
         AnnotationConfigApplicationContext applicationContext=new AnnotationConfigApplicationContext();
+        ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
+        UserFactory userFactory = new DefaultUserFactory();
+        beanFactory.registerSingleton("userFactory",userFactory);
         applicationContext.refresh();
 
-        AutowireCapableBeanFactory beanFactory = applicationContext.getAutowireCapableBeanFactory();
-        UserFactory userFactory = new DefaultUserFactory();
-        if(beanFactory instanceof SingletonBeanRegistry){
-            SingletonBeanRegistry registry = (SingletonBeanRegistry)beanFactory;
-            registry.registerSingleton("userFactory",userFactory);
-        }
         System.out.println(applicationContext.getBean("userFactory"));
         System.out.println(userFactory);
 
