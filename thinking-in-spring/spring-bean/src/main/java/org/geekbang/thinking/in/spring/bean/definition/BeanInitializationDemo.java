8a29b99341bbcc4d03a9a9026a262fdc8aa41da3
zhangrongzhao <rongzhao.zhang@allcure.cn>
2020-07-30 09:51:53 +0800

    bean：实例化(构造函数)=>初始化（@PostContruct,InitializingBean.afterPropertiesSet(),自定义初始化）=>销毁资源（@PreDestroy,DisposableBean.destory(),自定义销毁方法）=>finalize(GC垃圾回收内存)

--- thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/BeanInitializationDemo.java
+++ thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/BeanInitializationDemo.java
@@ -2,27 +2,39 @@ package org.geekbang.thinking.in.spring.bean.definition;
 
 import org.geekbang.thinking.in.spring.bean.definition.factory.DefaultUserFactory;
 import org.geekbang.thinking.in.spring.bean.definition.factory.UserFactory;
-import org.geekbang.thinking.in.spring.ioc.overview.dependency.domain.User;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 /*
  *
  */
 public class BeanInitializationDemo {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         AnnotationConfigApplicationContext applicationContext=new AnnotationConfigApplicationContext();
         applicationContext.register(BeanInitializationDemo.Config.class);
+        System.out.println("--------Spring Ioc Container start----begin-----");
         applicationContext.refresh();
+        System.out.println("--------Spring Ioc Container start-----end------");
 
         System.out.println(applicationContext.getBean(UserFactory.class));
+
+        System.out.println("--------Spring Ioc Container destroy----begin-----");
+        applicationContext.close();
+        System.out.println("--------Spring Ioc Container destroy-----end----");
+
+        System.out.println("--------garbage collection -----start-----------");
+        System.gc();
+        Thread.sleep(100);
+        System.out.println("--------garbage collection -----end-------------");
     }
 
     @Configuration
     public static class Config{
-        @Bean(initMethod = "initUserFactory")
+        @Bean(initMethod = "customInitUserFactory",destroyMethod = "customDestroy")
+        @Lazy(value=false)
         public UserFactory userFactory(){
             return new DefaultUserFactory();
         }
--- thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/factory/DefaultUserFactory.java
+++ thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/factory/DefaultUserFactory.java
@@ -1,22 +1,45 @@
 package org.geekbang.thinking.in.spring.bean.definition.factory;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-public class DefaultUserFactory implements UserFactory, InitializingBean {
+public class DefaultUserFactory implements UserFactory, InitializingBean, DisposableBean {
+
+    public DefaultUserFactory(){
+        System.out.println("0.first step:DefaultUserFactory 无参构造函数执行完成--实例化完成");
+    }
+
     //1.基于 @PostConstruct 注解实现
     @PostConstruct
-    public void init(){
-       System.out.println("@PostConstruct userFactory 初始化中...");
+    public void postConstruct(){
+        System.out.println("1.second step:@PostConstruct : userFactory 实例化后置初始化");
+    }
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("2.third step:afterPropertiesSet（InitializingBean）：userFactory初始化中...");
+    }
+    public void customInitUserFactory(){
+        System.out.println("3.fourth step:自定义初始化方法：customInitUserFactory...");
+    }
+
+
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("4.preDestroy step:准备销毁...");
+    }
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("5.destroy step:销毁中...");
+    }
+    public void customDestroy(){
+        System.out.println("6.custom destroy step:自定义销毁...");
     }
 
     @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("afterPropertiesSet：userFactory初始化中...");
-    }
-
-    public void initUserFactory(){
-       System.out.println("自定义初始化方法：initUserFactory...");
+    protected void finalize() throws Throwable {
+        System.out.println("7.GC...垃圾回收中...");
     }
 }
