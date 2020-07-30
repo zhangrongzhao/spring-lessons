3b5099ea0d8667cd7f61a410f3ddc64e4e906195
zhangrongzhao <rongzhao.zhang@allcure.cn>
2020-07-30 11:50:06 +0800

    提取公共依赖到父级项目中

---
+++ thinking-in-spring/dependency-lookup/src/main/java/org/geekbang/thinking/in/spring/dependency/lookup/ObjectProviderDemo.java
@@ -0,0 +1,11 @@
+package org.geekbang.thinking.in.spring.dependency.lookup;
+
+/**
+ * 通过ObjectProvider进行依赖查找
+ * **/
+public class ObjectProviderDemo {
+    public static void main(String[] args){
+
+
+    }
+}
