9825907692c01afc648d65385a65a24498b9a7f6
rongzhao.zhang <rongzhao.zhang@allcure.cn>
2020-08-02 10:46:27 +0800

    基本类型（枚举注入），数组注入，集合注入。

--- thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/dependency/domain/User.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/dependency/domain/User.java
@@ -1,8 +1,46 @@
 package org.geekbang.thinking.in.spring.ioc.overview.dependency.domain;
 
+import org.geekbang.thinking.in.spring.ioc.overview.dependency.enums.City;
+import org.springframework.core.io.Resource;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class User {
     private Long id;
     private String name;
+    private City city;
+    private Resource configFileLocation;
+    private City[] workCities;
+    private List<City> lifeCities;
+
+    public List<City> getLifeCities() {
+        return lifeCities;
+    }
+    public void setLifeCities(List<City> lifeCities) {
+        this.lifeCities = lifeCities;
+    }
+
+    public City[] getWorkCities() {
+        return workCities;
+    }
+    public void setWorkCities(City[] workCities) {
+        this.workCities = workCities;
+    }
+
+    public Resource getConfigFileLocation() {
+        return configFileLocation;
+    }
+    public void setConfigFileLocation(Resource configFileLocation) {
+        this.configFileLocation = configFileLocation;
+    }
+
+    public City getCity() {
+        return city;
+    }
+    public void setCity(City city) {
+        this.city = city;
+    }
 
     public Long getId() {
         return id;
@@ -18,19 +56,24 @@ public class User {
         this.name = name;
     }
 
-
     public static User createInstance(){
         User newUser=new User();
         newUser.setId(4L);
         newUser.setName("小马哥");
+        newUser.setCity(City.BEIJING);
         return newUser;
     }
+
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", city=" + city +
+                ", configFileLocation=" + configFileLocation +
+                ", workCities=" + Arrays.toString(workCities) +
+                ", lifeCities=" + lifeCities +
                 '}';
     }
 }
---
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/dependency/enums/City.java
@@ -0,0 +1,10 @@
+package org.geekbang.thinking.in.spring.ioc.overview.dependency.enums;
+
+/*
+* 城市
+* */
+public enum City {
+    BEIJING,
+    SHANGHAI,
+    HANGZHOU,
+}
