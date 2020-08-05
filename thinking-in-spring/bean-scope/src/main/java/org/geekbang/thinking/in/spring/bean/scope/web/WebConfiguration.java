dae4a8b9c0d28f034076ecd4b35881f9dfede04b
zhangrongzhao <rongzhao.zhang@allcure.cn>
2020-08-05 19:49:25 +0800

    session 作用域

--- thinking-in-spring/bean-scope/src/main/java/org/geekbang/thinking/in/spring/bean/scope/web/WebConfiguration.java
+++ thinking-in-spring/bean-scope/src/main/java/org/geekbang/thinking/in/spring/bean/scope/web/WebConfiguration.java
@@ -4,6 +4,7 @@ import org.geekbang.thinking.in.spring.ioc.overview.dependency.domain.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -14,7 +15,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class WebConfiguration {
 
     @Bean
-    @RequestScope
+    @SessionScope
     public User user(){
         User user=new User();
         user.setId(1L);
