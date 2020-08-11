1e1371fcb1271f29cd86e3b97bb1c9877c3831b0
zhangrongzhao <rongzhao.zhang@allcure.cn>
2020-08-11 19:02:05 +0800

    Aware接口

--- thinking-in-spring/bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/UserHolder.java
+++ thinking-in-spring/bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/UserHolder.java
@@ -2,31 +2,60 @@ package org.geekbang.thinking.in.spring.bean.lifecycle;
 
 
 import org.geekbang.thinking.in.spring.ioc.overview.dependency.domain.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 
 /**
  * User holder 类
  * */
-public class UserHolder {
+public class UserHolder implements BeanNameAware, BeanClassLoaderAware, BeanFactoryAware, EnvironmentAware {
     private User user;
     private Integer number;
     private String description;
 
+    private String beanName;
+    private ClassLoader classLoader;
+    private BeanFactory beanFactory;
+    private Environment environment;
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public UserHolder(User user){
+    public UserHolder(User user) {
         this.user = user;
     }
 
     public Integer getNumber() {
         return number;
     }
+
     public void setNumber(Integer number) {
         this.number = number;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
     }
 
     @Override
@@ -35,6 +64,12 @@ public class UserHolder {
                 "user=" + user +
                 ", number=" + number +
                 ", description='" + description + '\'' +
+                ", beanName='" + beanName + '\'' +
                 '}';
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 }
