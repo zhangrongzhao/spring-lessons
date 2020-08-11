d5596819fe6d7d71ba2877021e546aec23ccaa6a
zhangrongzhao <rongzhao.zhang@allcure.cn>
2020-08-11 17:45:00 +0800

    设置属性：applyPropertiesValues

--- thinking-in-spring/bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/BeanConstructorDependencyInjectionDemo.java
+++ thinking-in-spring/bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/BeanConstructorDependencyInjectionDemo.java
@@ -3,6 +3,9 @@ package org.geekbang.thinking.in.spring.bean.lifecycle;
 import org.geekbang.thinking.in.spring.ioc.overview.dependency.domain.SuperUser;
 import org.geekbang.thinking.in.spring.ioc.overview.dependency.domain.User;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.PropertyValue;
+import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
@@ -28,15 +31,15 @@ public class BeanConstructorDependencyInjectionDemo {
         int beanDefinitionCount = beanDefinitionAfter - beanDefinitionBefore;
         System.out.println("加载的BeanDefinition数量：" + beanDefinitionCount);
 
-        User user = beanFactory.getBean("user", User.class);
-        System.out.println(user);
+//        User user = beanFactory.getBean("user", User.class);
+//        System.out.println(user);
 //
 //        SuperUser superUser = (SuperUser) beanFactory.getBean("superUser", User.class);
 //        System.out.println(superUser);
 
 //        //构造器注入按照类型注入，resolveDependency
-//        UserHolder userholder = beanFactory.getBean("userHolder", UserHolder.class);
-//        System.out.println(userholder);
+        UserHolder userholder = beanFactory.getBean("userHolder", UserHolder.class);
+        System.out.println(userholder);
     }
 
     static class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
@@ -58,6 +61,37 @@ public class BeanConstructorDependencyInjectionDemo {
                 return false;
             }
             return true;
+        }
+
+        //user 是 跳过 Bean属性设置（填入）
+        //SuperUser 完整跳过 Bean属性设置（填入）
+        //useHolder
+        @Override
+        public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName)
+                throws BeansException {
+            //对userHolder进行拦截
+            if(ObjectUtils.nullSafeEquals("userHolder",beanName)&&UserHolder.class.equals(bean.getClass())){
+                //假设<property name="number" value="1"></property>配置，PropertyValues就包含元素PropertyValue(number=1)
+                final  MutablePropertyValues propertyValues;
+                if(pvs instanceof MutablePropertyValues){
+                    propertyValues=(MutablePropertyValues)pvs;
+                }else{
+                    propertyValues = new MutablePropertyValues();
+                }
+                propertyValues.addPropertyValue("number","1");
+
+                //如果存在description属性，则进行替换
+                if(propertyValues.contains("description")){
+                    //PropertyValue是不可变的
+                    //PropertyValue propertyValue = (PropertyValue) propertyValues.getPropertyValue("description");
+                    propertyValues.removePropertyValue("description");
+                    propertyValues.addPropertyValue("description","The user holder:v2");
+                }
+                //原始属性：<property name="description" value="the user holder"></property>
+
+                return propertyValues;
+            }
+            return null;
         }
     }
 }
--- thinking-in-spring/bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/UserHolder.java
+++ thinking-in-spring/bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/bean/lifecycle/UserHolder.java
@@ -8,18 +8,33 @@ import org.geekbang.thinking.in.spring.ioc.overview.dependency.domain.User;
  * */
 public class UserHolder {
     private User user;
+    private Integer number;
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public UserHolder(User user){
         this.user = user;
     }
-//
-//    public UserHolder() {
-//
-//    }
+
+    public Integer getNumber() {
+        return number;
+    }
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
 
     @Override
     public String toString() {
         return "UserHolder{" +
                 "user=" + user +
+                ", number=" + number +
+                ", description='" + description + '\'' +
                 '}';
     }
 }
