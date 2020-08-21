1560a71a51f87fb27c2c76ce30b4427fd27c5784
zhangrongzhao <rongzhao.zhang@allcure.cn>
2020-08-21 17:58:52 +0800

    Error接口

---
+++ thinking-in-spring/validation/src/main/java/org/geekbang/thinking/in/spring/validation/ValidatorDemo.java
@@ -0,0 +1,12 @@
+package org.geekbang.thinking.in.spring.validation;
+
+import org.springframework.validation.Validator;
+
+/**
+ * {@link Validator} 示例
+ * **/
+public class ValidatorDemo {
+    public static void main(String[] args){
+
+    }
+}
