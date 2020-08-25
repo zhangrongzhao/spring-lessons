bcdfbd3193c6080b2ee7d42f9cfa1ddc09fa7d27
zhangrongzhao <rongzhao.zhang@allcure.cn>
2020-08-25 11:47:54 +0800

    springframework底层：类型转换TypeConverter的API实现及流程，委托类：TypeConverterDelegate

--- thinking-in-spring/conversion/src/main/java/org/geekbang/thinking/in/spring/conversion/SpringCustomizedPropertyEditorDemo.java
+++ thinking-in-spring/conversion/src/main/java/org/geekbang/thinking/in/spring/conversion/SpringCustomizedPropertyEditorDemo.java
@@ -15,9 +15,16 @@ public class SpringCustomizedPropertyEditorDemo {
     public static void main(String[] args){
         ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/property-editors-context.xml");
 
+        //AbstractApplicationContext->"conversionService" ConversionService Bean
+        //->ConfigurableBeanFactory#setConversionService(conversionService)
+        //->AbstractAutowiredBeanFactory.instantiateBean
+        //->AbstractBeanFactory#getConversionService->BeanWrapper
+        //BeanDefinition->BeanWrapper->属性转换（数据来源） PropertyValues
+        //setPropertyValues(PropertyValues)->typeConverter#convertIfNecessnary
+        //TypeConverterDelegate#convertIfNecessary->PropertyEditor or ConversionService
+
         User user = applicationContext.getBean("user",User.class);
         System.out.println(user);
-
 
         applicationContext.close();
     }
