2b8e6611aacd22112f902eb3f5683c298e509134
zhangrongzhao <rongzhao.zhang@allcure.cn>
2020-09-01 18:07:57 +0800

    组件扫描

---
+++ thinking-in-spring/annotation/src/main/java/org/geekbang/thinking/in/spring/annotation/ComponentScanDemo.java
@@ -0,0 +1,13 @@
+package org.geekbang.thinking.in.spring.annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 组件扫描示例
+ * */
+@ComponentScan(basePackages="")
+public class ComponentScanDemo {
+    public static void main(String[] args){
+
+    }
+}
